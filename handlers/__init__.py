410b6c4a6de60f35f05fa9b3cf5ecb7f37031c02
rifqy1146 <rifqikunduran@gmail.com>
2025-12-29 15:26:40 +0700

    osndndj

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,9 +3,11 @@ from .speedtest import register as speedtest_register
 from .menu import register as menu_register
 from .alive import register as alive_register
 from .info import register as info_register
+from .qr import register as qr_register
 
 def load_handlers(app):
     ping_register(app)
+    qr_register(app)
     info_register(app)
     speedtest_register(app)
     menu_register(app)
